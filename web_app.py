22d88020783826493fb637997c1b1e6d465b038a
shawnljw <thisisshawnlee@gmail.com>
2024-06-16 19:41:12 +0800

    fix clear and label buttons

--- web_app.py
+++ web_app.py
@@ -4,12 +4,12 @@ import numpy as np
 import plotly.graph_objects as go
 import gen_utils as gu
 import open3d as o3d
-from dash import Dash, html, dcc, callback, Output, Input, State
+from dash import Dash, html, ctx, dcc, callback, Output, Input, State, MATCH, ALL
+from dash.exceptions import PreventUpdate
 import trimesh
 from inference_pipelines.inference_pipeline_maker import make_inference_pipeline
 
-def predict_teeth(mesh):
-    return np.random.randint(0, 9, len(mesh.vertices))
+mesh, pipeline = None, None
 
 def get_plotly_mesh(mesh):
     triangles = np.asarray(mesh.triangles)
@@ -27,7 +27,7 @@ def get_plotly_mesh(mesh):
         vertexcolor=vertexcolor,
         flatshading=True,
         lighting=dict(
-            ambient=0.18,
+            ambient=0.25,
             diffuse=1,
             fresnel=0.1,
             specular=1,
@@ -49,13 +49,21 @@ def get_plotly_mesh(mesh):
 
 @callback(
     Output('mesh-container', 'children'),
+    Input({"type": "clear-button", "index": ALL}, "n_clicks"),
     Input('upload-button', 'contents'),
     State('upload-button', 'filename'),
+    prevent_initial_call = True
 )
-def upload_mesh(contents, filename):
+def update_graph(n_clicks, contents, filename):
+    triggered_id = ctx.triggered_id
+    if triggered_id == "upload-button":
+        return read_mesh(contents, filename)
+    else:
+        return reset_graph(n_clicks)
+    
+
+def read_mesh(contents, filename):
     global mesh
-    if contents is None:
-        return html.Div(children='Upload a 3D mesh to visualise it', id="mesh-container", style={'textAlign':'center'})
     content_type, content_string = contents.split(',')
     mesh_path = io.BytesIO(base64.b64decode(content_string))
     trimesh_mesh = trimesh.load(mesh_path, file_type=filename.split('.')[-1])
@@ -65,37 +73,55 @@ def upload_mesh(contents, filename):
     mesh.vertices = o3d.utility.Vector3dVector(vertex_ls)
     mesh.triangles = o3d.utility.Vector3iVector(tri_ls)
     mesh.compute_vertex_normals()
-    return dcc.Graph(figure=get_plotly_mesh(mesh), id="mesh-object")
+    graph = dcc.Graph(
+        figure=get_plotly_mesh(mesh),
+        id={"type": "mesh-object", "index": 0}
+    )
+    clear_button = html.Button(
+        "Clear",
+        style={'margin':'auto', 'display':'block'},
+        id={"type": "clear-button", "index": 0}
+    )
+    label_button = html.Button(
+        "Label Teeth",
+        style={'margin':'auto', 'display':'block'},
+        id={"type": "label-button", "index": 0}
+    ) 
+    return [graph, clear_button, label_button]
+
+def reset_graph(n_clicks):
+    global mesh
+    mesh = None
+    return "Upload a 3D mesh to visualise it"
+        
 
 @callback(
-    Output('mesh-object', 'figure'),
-    Input("label-button", "n_clicks"),
+    Output({"type": "mesh-object", "index": MATCH}, 'figure'),
+    Input({"type": "label-button", "index": MATCH}, "n_clicks"),
+    prevent_initial_call=True
 )
 def label_teeth(n_clicks):
-    global mesh
-    if mesh is not None:
-        print("Labelling teeth")
-        outputs = pipeline(mesh)
-        lables = np.array(outputs["sem"])
-        mesh = gu.get_colored_mesh(mesh, lables)
-        return get_plotly_mesh(mesh)
+    global mesh, pipeline
+    # load pipeline on first run
+    if pipeline is None:
+        pipeline = make_inference_pipeline(
+            model_name="tgnet",
+            ckpt_path_ls=["ckpts/tgnet_fps.h5", "ckpts/tgnet_bdl.h5"],
+        )
+            
+    outputs = pipeline(mesh)
+    lables = np.array(outputs["sem"])
+    mesh = gu.get_colored_mesh(mesh, lables)
+    return get_plotly_mesh(mesh)
 
-pipeline = make_inference_pipeline(
-    model_name="tgnet",
-    ckpt_path_ls=["ckpts/tgnet_fps.h5", "ckpts/tgnet_bdl.h5"],
-)
-
-mesh = None
 app = Dash(__name__)
 app.layout = [
     html.H1(children='Teeth Segmentation', style={'textAlign':'center'}),
-    html.Button("Clear", id="clear-button", style={'margin':'auto', 'display':'block'}),
     dcc.Upload(
         id='upload-button',
         children=html.Button('Upload Data', style={'margin':'auto', 'display':'block'}),
     ),
-    html.Div(children='Upload a 3D mesh to visualise it', id="mesh-container", style={'textAlign':'center'}),
-    html.Button("Label Teeth", id="label-button", style={'margin':'auto', 'display':'block'}),
+    html.Div("Upload a 3D mesh to visualise it", id="mesh-container", style={'textAlign':'center'}),
 ]
 
 if __name__ == '__main__':
