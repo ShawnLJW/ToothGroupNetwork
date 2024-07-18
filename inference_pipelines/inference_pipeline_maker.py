cde0ddd78bd0b28639d62452aabba4a9aefe6dc3
shawnljw <github@shawnljw.com>
2024-07-19 00:14:25 +0800

    bug fix

--- inference_pipelines/inference_pipeline_maker.py
+++ inference_pipelines/inference_pipeline_maker.py
@@ -82,23 +82,9 @@ def make_inference_pipeline(model_name, ckpt_path_ls):
         module.cuda()
         return InferencePipeLine(module)
     elif model_name=="pointtransformer":
-        inference_config = {
-            "model_info":{
-                "model_parameter" :{
-                    "input_feat": 6,
-                    "stride": [1, 4, 4, 4, 4],
-                    "nstride": [2, 2, 2, 2],
-                    "nsample": [36, 24, 24, 24, 24],
-                    "blocks": [2, 3, 4, 6, 3],
-                    "block_num": 5,
-                    "planes": [32, 64, 128, 256, 512],
-                    "crop_sample_size": 3072,
-                },
-            },
-        }
         from inference_pipelines.inference_pipeline_sem import InferencePipeLine
         from models.point_transformer import PointTransformer
-        module = PointTransformer(inference_config["model_info"])
+        module = PointTransformer()
         module.load_state_dict(torch.load(ckpt_path_ls[0]))
         module.cuda()
         return InferencePipeLine(module)
